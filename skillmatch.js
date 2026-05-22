760233f2bc800ca976d92e1728c1dd0c745183ff
Almir Dambroski <61130910+almirdambroski@users.noreply.github.com>
2026-05-21 21:42:56 -0300

    feat: adiciona dados do candidato e vagas

---
+++ skillmatch.js
@@ -0,0 +1,38 @@
+//RF01 – Criar o perfil do candidato
+//Objeto simples usado
+
+const candidato = {
+    nome: "Almir",
+    area: "Front-End",
+    habilidades: ["JavaScript", "GitHub", "Lógica de Programação", "Kanban"],
+    experienciaMeses: 3
+};
+
+//RF02 – Criar uma lista de vagas
+//array com pelo menos 03 vagas fictícias.
+const vagas = [
+    {
+    id: 1,
+    empresa: "TechStart",
+    cargo: "Desenvolvedor Front-End Júnior",
+    requisitos: ["JavaScript", "GitHub", "Lógica de Programação"],
+    salario: 2800,
+    modalidade: "Remoto"
+    },
+    {
+    id: 2,
+    empresa: "CodeLab",
+    cargo: "Estágio Front-End",
+    requisitos: ["JavaScript", "Kanban", "GitHub"],
+    salario: 1800,
+    modalidade: "Híbrido"
+    },
+    {
+    id: 3,
+    empresa: "WebSolutions",
+    cargo: "Programador JavaScript Júnior",
+    requisitos: ["JavaScript", "Arrays", "Objetos", "Funções"],
+    salario: 3000,
+    modalidade: "Presencial"
+    }
+];
